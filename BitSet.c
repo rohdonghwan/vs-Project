5d66152244c6fc8ee405ae4cec7d91e8406ca4c9
동환 노 <laborh0129@cu.ac.kr>
2018-11-30 11:49:32 +0900

    .

---
+++ BitSet.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include "BitSet.h"
+
+int IsMember(BitSet s, int n)
+{
+	return s & SetOf(n);
+}
+
+void Add(BitSet *s, int n)
+{
+	*s |= SetOf(n);
+}
+
+void Remove(BitSet *s, int n)
+{
+	*s &= ~SetOf(n);
+}
+
+int Size(BitSet s)
+{
+	int n = 0;
+	for (; s != BitSetNull; n++)
+		s &= s - 1;
+	return n;
+}
+
+void Print(BitSet s)
+{
+	int i;
+	printf("{ ");
+	for (i = 0; i < BitSetBits; i++)
+		if (IsMember(s, i))
+			printf("%d ", i);
+	printf("}");
+}
+
+void PrintLn(BitSet s)
+{
+	Print(s);
+	putchar('\n');
+}
